eb1dd9187d134ea838449e892a9f038c0fd0a97c
juand98 <juanduartet.98@gmail.com>
2022-07-12 13:32:53 -0500

    extensión

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import data from "./data.json";
 import "./App.css";
 
 function App() {
+  //Usaremos el hook "useState", que nos permitirá inicializar y cambiar cierta información o estado relacionado al componente. Gracias a la extensión automáticamente este se importa en nuestro archivo. Recordemos que en los corchetess, como primer parámetro asignamos un nombre a la información con la cual vamos a trabajar, en este caso todos (tareas, english). Como segundo parámetro usamos el set para poder cambiar o actulizar la lista de tareas.
+  const [todos, setTodos] = useState(data);
+
+  console.log(todos);
   return (
-    <div className="App">
-      <h1>Hola</h1>
+    <div className="container">
+      <h2>Hola</h2>
     </div>
   );
 }
